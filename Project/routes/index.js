1752a1067d8ea18ce6ad0913385a3a38511f0ce9
Laiba Rana <laibarana123@outlook.com>
2022-05-19 09:34:02 +0500

    Updated

--- Project/routes/index.js
+++ Project/routes/index.js
@@ -15,4 +15,44 @@ router.get("/dashboard", ensureAuthenticated, (req, res) =>
   })
 );
 
+// Gallery
+router.get("/gallery", ensureAuthenticated, (req, res) =>
+  res.render("gallery", {
+    user: req.user,
+    layout: "layouts/layout"
+  })
+);
+
+// Dropdown1
+router.get("/dropdown1", ensureAuthenticated, (req, res) =>
+  res.render("dropdown1", {
+    user: req.user,
+    layout: "layouts/layout"
+  })
+);
+
+// Dropdown2
+router.get("/dropdown2", ensureAuthenticated, (req, res) =>
+  res.render("dropdown2", {
+    user: req.user,
+    layout: "layouts/layout"
+  })
+);
+
+// contact
+router.get("/contact", ensureAuthenticated, (req, res) =>
+  res.render("contact", {
+    user: req.user,
+    layout: "layouts/layout"
+  })
+);
+
+// order
+router.get("/order", ensureAuthenticated, (req, res) =>
+  res.render("order", {
+    user: req.user,
+    layout: "layouts/layout"
+  })
+);
+
 module.exports = router;
